5df0cf57e7f9796b0cf639e829d7c399d8b78b94
Miguel Cabrera <corderom776@gmail.com>
2024-12-07 12:56:38 -0600

    act

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(cors({ origin: 'https://miguelcg17.github.io' }));
+app.use(cors());
 
 
 app.use(express.json());
